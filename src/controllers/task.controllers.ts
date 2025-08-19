04843fcd0963ce92a4a0f1853e87aefde025a49d
RionakAsume <imlauerpablo@gmail.com>
2025-08-19 04:39:44 -0300

    cambios que parece que no subi

--- src/controllers/task.controllers.ts
+++ src/controllers/task.controllers.ts
@@ -3,6 +3,7 @@ import {
     createTaskHandler,
     getAllTasksHandler,
     getTaskByIdHandler,
+    getTasksByGeneralTaskIdHandler,
     updateTaskHandler,
     deleteTaskHandler,
 } from "../handlers/task";
@@ -50,6 +51,19 @@ export const getTaskById = async (req: Request, res: Response) => {
     }
 };
 
+export const getTasksByGeneralTaskId = async (req: Request, res: Response) => {
+    try {
+        const { generalTaskId } = req.params;
+        const tasks = await getTasksByGeneralTaskIdHandler(Number(generalTaskId));
+        res.status(200).json({ data: tasks });
+    } catch (error) {
+        console.error("Error en el controlador al obtener las tareas por ID de tarea general:", error);
+        res.status(500).json({ error: "Error al obtener las tareas por ID de tarea general." });
+    }
+};
+
+
+
 /**
  * Actualiza una tarea por su ID.
  */
@@ -77,7 +91,7 @@ export const deleteTask = async (req: Request, res: Response) => {
         if (result === 0) {
             return res.status(404).json({ error: "Tarea no encontrada para eliminar." });
         }
-        res.status(204).send(); // Código 204 No Content para eliminación exitosa
+        res.status(204).send('Tarea eliminada'); // Código 204 No Content para eliminación exitosa
     } catch (error) {
         console.error("Error en el controlador al eliminar la tarea:", error);
         res.status(500).json({ error: "Error al eliminar la tarea." });
--- src/models/Materials.models.ts
+++ src/models/Materials.models.ts
@@ -1,40 +1,40 @@
 import {
-  Table,
-  Column,
-  Model,
-  PrimaryKey,
-  AutoIncrement,
-  DataType,
-  HasMany,
+  Table,
+  Column,
+  Model,
+  PrimaryKey,
+  AutoIncrement,
+  DataType,
+  HasMany,
   BelongsToMany // Importa BelongsToMany
 } from 'sequelize-typescript';
 import MaterialsTasks from './Materials_tasks.models';
 import Task from './Task.models'; // Importa el modelo Task
 
 @Table({
-  tableName: 'materials',
-  timestamps: false
+  tableName: 'materials',
+  timestamps: false
 })
 class Material extends Model {
-  @PrimaryKey
-  @AutoIncrement
-  @Column({
-    type: DataType.INTEGER,
-  })
-  ID_materials!: number;
+  @PrimaryKey
+  @AutoIncrement
+  @Column({
+    type: DataType.INTEGER,
+  })
+  ID_materials!: number;
 
-  @Column({
-    type: DataType.STRING(100),
-  })
-  name!: string;
+  @Column({
+    type: DataType.STRING(100),
+  })
+  name!: string;
 
-  @Column({
-    type: DataType.STRING(100),
-  })
-  type!: string;
+  @Column({
+    type: DataType.STRING(100),
+  })
+  type!: string;
 
-  @HasMany(() => MaterialsTasks)
-  materialsTasks!: MaterialsTasks[];
+  @HasMany(() => MaterialsTasks)
+  materialsTasks!: MaterialsTasks[];
 
   // Relación Muchos a Muchos con Task a través de MaterialsTasks
   @BelongsToMany(() => Task, () => MaterialsTasks)
