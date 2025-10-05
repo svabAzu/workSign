f1ecb9027cc2141a6216e9c734e3e872c611a91d
svabAzu <svab.azu@gmail.com>
2025-10-05 00:18:02 -0300

    Refactorizacion de estructura del código para conecciones en el front para editar usuarios

--- src/router/users.router.ts
+++ src/router/users.router.ts
@@ -1,5 +1,6 @@
 import {Router} from "express"
-import { postUser,getUser,getUserForId,putUserForId, getOperators } from "../handlers/users";
+import { postUser,getUser,getUserForId,putUserForId, getOperators,  } from "../handlers/users";
+import uploadAvatar from "../middlewares/multerAvatar.middleware";
 
 const routerUser= Router();
 
@@ -7,6 +8,7 @@ routerUser.post('/', postUser),
 routerUser.get('/', getUser),
 routerUser.get('/operators/all', getOperators);
 routerUser.get('/:id',  getUserForId)
-routerUser.put('/:id', putUserForId);
+routerUser.put('/:id', uploadAvatar.single('avatar_url'), putUserForId);
+
 
 export default routerUser
