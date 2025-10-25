d0603f8a05318978b91cbcdcf724c8bff15bb55a
RionakAsume <imlauerpablo@gmail.com>
2025-10-25 18:39:27 -0300

    configuracion de cors y validaciones por seguridad

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,8 +1,21 @@
 
 import cors from 'cors';
 
-const corsOptions = {
-    origin: 'http://localhost:5173',
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://pabloproyectos.com.ar',
+    'https://www.pabloproyectos.com.ar',
+    'https://app.pabloproyectos.com.ar', // Agrega aquí tu dominio de producción si es diferente
+];
+
+const corsOptions: cors.CorsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 };
 
--- src/router/users.router.ts
+++ src/router/users.router.ts
@@ -3,18 +3,20 @@ import { postUser, getUser, getUserForId, putUserForId, getOperators, putUserSta
 import uploadAvatar from "../middlewares/multerAvatar.middleware";
 import { validateSchema } from "../middlewares/validator.middleware";
 import { registerSchema, updateUserSchema } from "../schemas/auth.schema";
+import { authRequierd } from "../middlewares/validateToken";
 
 const routerUser = Router();
 
-routerUser.post('/', validateSchema(registerSchema), postUser),
-    routerUser.get('/', getUser),
-    routerUser.get('/operators/all', getOperators);
-routerUser.get('/:id', getUserForId)
+routerUser.post('/', authRequierd, validateSchema(registerSchema), postUser),
+    routerUser.get('/', authRequierd, getUser),
+    routerUser.get('/operators/all', authRequierd, getOperators);
+routerUser.get('/:id', authRequierd, getUserForId)
 
 // Ruta para dar de baja usuario (cambiar state a false)
 routerUser.put('/state/:id', putUserState);
 
 routerUser.put('/:id', uploadAvatar.single('avatar_url'),
+    authRequierd,
     //validateSchema(updateUserSchema), 
     putUserForId);
 
